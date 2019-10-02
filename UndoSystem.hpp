f2f0c160b74da4a6896bf397439aff4a79e84edf
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-10-02 15:38:38 +0900

    check undo max

--- UndoSystem.hpp
+++ UndoSystem.hpp
@@ -1,7 +1,7 @@
 // UndoSystem.hpp --- Undo/Redo framework for C++11
 // Copyright (C) 2019 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>.
 #ifndef UNDO_SYSTEM_HPP_
-#define UNDO_SYSTEM_HPP_    4   // Version 4
+#define UNDO_SYSTEM_HPP_    5   // Version 5
 
 #include <deque>    // for std::deque
 #include <memory>   // for std::shared_ptr
@@ -55,11 +55,27 @@ struct UndoSystem
         return false;
     }
 
+    virtual std::size_t get_undo_max() const
+    {
+        return 256;
+    }
+
+    void check_undo_max()
+    {
+        if (m_undo_index > 0 &&
+            m_undo_buffer.size() > get_undo_max())
+        {
+            m_undo_buffer.pop_front();
+            --m_undo_index;
+        }
+    }
+
     void add_undo(UndoData *data)
     {
         m_undo_buffer.resize(m_undo_index);
         m_undo_buffer.emplace_back(data);
         ++m_undo_index;
+        check_undo_max();
     }
 
     void add_undo(std::shared_ptr<UndoData> data)
@@ -67,6 +83,7 @@ struct UndoSystem
         m_undo_buffer.resize(m_undo_index);
         m_undo_buffer.push_back(data);
         ++m_undo_index;
+        check_undo_max();
     }
 
     void clear_undo_buffer()
